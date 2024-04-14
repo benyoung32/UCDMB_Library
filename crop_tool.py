2f423631726e38501df71919baa27f2da991b192
Abcdefgdude <benyoung32@gmail.com>
2024-04-14 12:49:13 -0700

    refactoring and making GUI more intuitive

--- crop_tool.py
+++ crop_tool.py
@@ -3,7 +3,9 @@ import os
 import tkinter as tk
 from tkinter import font
 import tkinter.filedialog
+from typing import Any, Callable
 import fitz
+from fontTools.ttLib.tables.S__i_l_f import Pass
 import pdf_reader as reader
 from PIL import Image,ImageTk
 MYFONT = ('Arial',16)
@@ -32,94 +34,85 @@ def createPageScaledImage(parent, page: fitz.Page, dpi = 300, resize:fitz.Point 
 
 class CropTool(tk.Toplevel):
     def __init__(self,parent,filepath="") -> None:
+        # create top level window
         super().__init__(parent)
         self.title("Crop pdf")
-        self.geometry("1000x1000")
-        self.history = []
+        self.geometry("1000x1000") 
+        
+        self.key_history = [] # this is for tracking multiple keyboard inputs at once
+        # these are the variables affected by the GUI entries
         self.keyvar = tk.StringVar()
         self.rotate_var = tk.IntVar(self)
         self.rightalign_var = tk.BooleanVar(self)
-        # self.fullsize_var = tk.BooleanVar(self)
         self.expand_var = tk.BooleanVar(self)
         self.twoinone_var = tk.BooleanVar(self)
         self.CROPBOX = None
         
-        if not filepath:
-            self.path = self.openFilePrompt()
-        else:
-            self.path = filepath
-            
-        doc = reader.openDocuments([self.path])[0]
-        page = doc[0]
-        self.page_br = page.bound().br
-        # set up canvases 
-        self.cv_width, self.cv_height = (self.page_br.x * SCALE,self.page_br.y * SCALE)
+        # create GUI frames
         self.main_frame = tk.Frame(master=self,
                     width=1000 * 2 + PADDING * 4, 
-                    height = 1000 + PADDING * 4,bg='gray')
+                    height = 1000 + PADDING * 4,bg='gray') 
+        self.entry_frame = tk.Frame(master=self.main_frame,bg='green')
+        # open file to display and crop
         self.openFile(filepath)
-        size = 1000
-        self.button_frame = tk.Frame(master=self.main_frame,
-                    bg='green',height=10,width =self.cv_width)
-        # preview_canvas.grid(row=0,column=1,sticky='ew',padx=PADDING,pady=PADDING)
-        self.preview_button = tk.Button(self.button_frame, command=self.createPreviewWindow,
-                                width = 10, height = 1,text='Create Preview')
-        self.export_button = tk.Button(self.button_frame, command=self.printSettingsString,
-                                width = 10, height = 1,text='Export Settings')
-        self.crop_folder_button = tk.Button(self.button_frame, command=self.applyCropToFolder,
-                                width = 20, height = 1, text = 'Apply to folder')
-        self.crop_file_button = tk.Button(self.button_frame, command=self.applyCropToFile, width = 20, height = 1,
-                                          text = 'Apply to file')
-        self.open_file_button = tk.Button(self.button_frame, command=self.openFile, text = 'Open file',
-                                          width = 20, height = 1);
-        self.rotate_entry = EntryWithLabel(self.button_frame,'Rotate?','')
-        # self.fullsize_box = CheckButtonWithLabel(self.button_frame, 
-        #                                         'Full Size?',self.fullsize_var )
-        self.expand_box = CheckButtonWithLabel(self.button_frame,
-                                                'Expand?', self.expand_var)
-        self.twoinone_box = CheckButtonWithLabel(self.button_frame,
-                                                'Two in one?',self.twoinone_var)
-        self.rightalign_box = CheckButtonWithLabel(self.button_frame, 
-                                                'Right Align?', self.rightalign_var)
-        self.button_frame.grid(row=0,column=1,sticky='nw',pady=PADDING,padx=PADDING)
-        self.preview_button.grid(row=0,column=0,sticky='nwes')
-        self.export_button.grid(row=0,column=1,sticky='nwes')
-        
 
-        
+        self.initEntries()
 
-        self.crop_file_button.grid(row=6,column=0, sticky='nwes')
-        self.crop_folder_button.grid(row=7,column=0,sticky='nwes')
-        
-        
-        self.rotate_entry.grid(row=1,column=0,sticky='nwes',columnspan=2)
-        # self.fullsize_box.grid(row=2,column=0,sticky = 'nwes',columnspan=2)
-        self.expand_box.grid(row=3,column=0,sticky = 'nwes',columnspan=2)
-        self.twoinone_box.grid(row=4,column=0,sticky = 'nwes',columnspan=2)
-        self.rightalign_box.grid(row=5,column=0,sticky = 'nwes',columnspan=2)
-        self.main_frame.pack(padx=PADDING,pady=PADDING,fill='none')
+        self.entry_frame.pack(fill='both', pady= PADDING,side = 'right',expand = True)
+        self.main_frame.pack(padx=PADDING,pady=PADDING,fill='both')
 
         self.after(30,self.updateKeyboardInput)
         self.bind("<Destroy>", self.kill_root)
         self.mainloop()
 
+    def initEntries(self) -> None:
+        self.latest_row = 0
+        self.preview_button = self.addEntry(tk.Button, command=self.createPreviewWindow,
+                                text='Preview Result')
+        self.print_button = self.addEntry(tk.Button, command=lambda:print(self.getSettingsString()),
+                                text='Print Settings')
+        self.crop_folder_button = self.addEntry(tk.Button, command=self.applyCropToFolder,
+                                text = 'Crop folder')
+        self.crop_file_button = self.addEntry(tk.Button, command=self.applyCropToFile, width = 20, height = 1,
+                                text = 'Crop file')
+        self.open_file_button = self.addEntry(tk.Button, command=self.openFile, 
+                                text = 'Open file',);
+        self.rotate_entry = self.addEntry(EntryWithLabel,label='Rotate?',
+                                default='')
+        self.expand_box = self.addEntry(CheckButtonWithLabel,
+                                label='Expand?', var=self.expand_var)
+        self.twoinone_box = self.addEntry(CheckButtonWithLabel,
+                                label='Two in one?',var=self.twoinone_var)
+        self.rightalign_box = self.addEntry(CheckButtonWithLabel, 
+                                label='Right Align?', var=self.rightalign_var)
+
+    def addEntry(self, entry_type: Callable, column:int = 0, columnspan:int = 1, **args) -> tk.Widget:
+        entry = entry_type(self.entry_frame, **args)
+        entry.grid(row = self.latest_row, column=column,
+                   sticky='nwes',columnspan=columnspan)
+        self.latest_row = self.latest_row + 1
+        return entry
+    
     def initPDFCanvas(self) -> None:
-        
-        doc = reader.openDocuments([self.path])[0]
+        doc = reader.openDocuments([self.filepath])[0]
         page = doc[0]
         self.page_br = page.bound().br
-        # set canvas 
+
+        # create canvas within main window 
         self.cv_width, self.cv_height = (self.page_br.x * SCALE,self.page_br.y * SCALE)
         self.pdf_canvas = tk.Canvas(master=self.main_frame, 
                     width = self.cv_width, height=self.cv_height, bg='yellow')
-        self.pdf_canvas.grid(row=0,column=0,sticky='ew',padx=PADDING,pady=PADDING)
+        self.pdf_canvas.pack(fill='both',side='left')
+
         # draw document to canvas
         self.myimage = createPageScaledImage(self,page,resize=page.bound().br)
         self.pdf_canvas.create_image(0,0,image=self.myimage,anchor='nw')
+        
         # init selection box as max size
         self.tl = fitz.Point(2,2)
         self.br = copy(self.page_br)
         
+        # add mouse and keyboard listeners for drawing crop box
         self.pdf_canvas.bind('<KeyPress>', self.keydown)
         self.pdf_canvas.bind('<KeyRelease>', self.keyup)
         self.pdf_canvas.bind('<Button-1>', self.onClick)
@@ -153,12 +146,9 @@ class CropTool(tk.Toplevel):
         self.br.y = event.y
         self.drawCropBox()
 
-    def setFile(self, str) -> None:
-
-
-    def openFilePrompt(self, filepath:str = "" ) -> None:
+    def openFile(self, filepath:str = "" ) -> None:
         if (filepath == ""): filepath = self.openFilePrompt()
-        self.path = filepath
+        self.filepath = filepath
         self.initPDFCanvas()
         pass
 
@@ -187,12 +177,12 @@ class CropTool(tk.Toplevel):
         self.CROPBOX = self.pdf_canvas.create_rectangle(self.tl.x, 
                         self.tl.y, self.br.x, self.br.y,outline='dodger blue',width=1)
 
-    def getSettingsDict(self) -> dict[str, any]:
+    def getSettingsDict(self) -> dict[str, Any]:
+        # build dict to describe the requested crop operation 
         args = {}
         tl, br = self.tl, self.br
-        args['filename'] = self.path
+        args['filename'] = self.filepath
         args['margins'] = (int(tl.y), int(tl.x) + 10, int(self.page_br.x - br.x) + 10, int(self.page_br.y - br.y))
-        # args['full_size'] = self.fullsize_var.get()
         args['full_size'] = True
         args['two_in_one'] = self.twoinone_var.get()
         args['expand'] = self.expand_var.get()
@@ -201,9 +191,10 @@ class CropTool(tk.Toplevel):
         return args
 
     def getSettingsString(self) -> str:
+        # print setting string  
         global page_br
         args = self.getSettingsDict()
-        out = '\"' + self.path + '\"' 
+        out = '\"' + self.filepath + '\"' 
         for v in ['full_size','two_in_one','expand','right_align']:
             if args[v]:
                 out += ' -' + v
@@ -217,12 +208,12 @@ class CropTool(tk.Toplevel):
     
     def applyCropToFolder(self) -> None:
         ''' apply current settings to the folder where the sample came from '''
-        folder= os.path.dirname(self.path)
+        folder= os.path.dirname(self.filepath)
         files = reader.getSubFiles(folder)
         reader.openCropSaveDocs(files, reader.prefix, **self.getSettingsDict())
     
     def applyCropToFile(self) -> None:
-        reader.openCropSaveDocs(self.path, reader.prefix, **self.getSettingsDict())
+        reader.openCropSaveDocs(self.filepath, reader.prefix, **self.getSettingsDict())
     
     # get rotation from entry box, sanitize input
     def getRotation(self) -> int:
@@ -237,7 +228,7 @@ class CropTool(tk.Toplevel):
 
     def createPreviewWindow(self) -> None:
         args = self.getSettingsDict()
-        doc = reader.openDocuments(self.path,size='a4')[0]
+        doc = reader.openDocuments([self.filepath],size='a4')[0]
         # alter document using settings 
         newdoc = reader.createCroppedDocument(doc,**args)
         # create new window to show altered document
@@ -248,14 +239,14 @@ class CropTool(tk.Toplevel):
             self.master.destroy()
     
     def keyup(self,event) -> None:
-        if event.keysym in self.history:
-            self.history.pop(self.history.index(event.keysym))
-            self.keyvar.set(str(self.history))
+        if event.keysym in self.key_history:
+            self.key_history.pop(self.key_history.index(event.keysym))
+            self.keyvar.set(str(self.key_history))
 
     def keydown(self, event) -> None:
-        if not event.keysym in self.history:
-            self.history.append(event.keysym)
-            self.keyvar.set(str(self.history))
+        if not event.keysym in self.key_history:
+            self.key_history.append(event.keysym)
+            self.keyvar.set(str(self.key_history))
 
 class EntryWithLabel(tk.Frame):
     '''
